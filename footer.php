122b4f55df4335241afc7fcd9afd8f3f8a0f6bb7
Anish Kunwar <anishkunwar3913@gmail.com>
2018-11-01 21:51:50 +0545

    project initiated

---
+++ footer.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="keywords" content="footer, address, phone, icons" />
+
+	<title>Footer </title>
+	<link rel="stylesheet" href="footer.css">
+	<link href="http://fonts.googleapis.com/css?family=Cookie" rel="stylesheet" type="text/css">
+</head>
+	<body>
+		<footer class="footer-distributed">
+     
+			<div class="footer-left">
+
+				<h3>Support</h3>
+				<hr />
+				<p class="footer-links">
+					<a href="#">Guides</a>
+					·
+					<a href="#">Privacy</a>
+					·
+					<a href="#">Policy</a>
+				</p>
+
+				<p class="footer-company-name">Copyright &copy 2018 PR Site.All Rights Reserved. Designed for change.</p>
+			</div>
+			<div class="footer-right">
+				<img src="./images/pr.png" height="70px" width="70px" alt="Logo logo">
+				<p class="footer-company-about">
+					<span>About the company</span>
+					Lorem ipsum dolor sit amet, consectateur adispicing elit. Fusce euismod convallis velit, eu auctor lacus vehicula sit amet.
+				</p>
+
+			</div>
+
+		</footer>
+
+	</body>
+
+</html>
